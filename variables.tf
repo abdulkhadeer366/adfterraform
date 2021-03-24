38e76fd95898af5e385ba15b76af8aa7b55abf91
Moghul Abdul Khadeer Baig <abdul.khadeer@ATMECS.CORP>
2021-03-24 21:50:52 +0530

    first-commit-for-adf-terraform-ci-cd

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "rgname" {
+  default = "Khadeer-varian-RG"
+}
+
+variable "location" {
+  default = "southindia"
+
+}
+variable "adfname" {
+  default = "khadeer-data-factory-varian"
+
+}
+
+variable "storage" {
+  default = "testvarianstorage"
+}
